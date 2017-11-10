e2909d17f29b92a1ec4ede542ebb7816393ae457
jweyn <33530169+jweyn@users.noreply.github.com>
2017-11-09 17:06:41 -0800

    Update engine.py

--- bin/engine.py
+++ bin/engine.py
@@ -7,3 +7,12 @@
 '''
 Main engine for the theta-e system.
 '''
+
+# Step 1: check the database
+# Step 2: if database has no table for the stid or data are old, reset the table
+# Step 3: if applicable, run db_init for site: retrieves historical
+#   We may need to write a separate script to initialize a new model
+# Step 4: retrieve forecast data; save to db
+# Step 5: retrieve verification data; save to db
+# Step 6: run manager to calculate verification statistics; save to db
+# Step 7: run plotting scripts; theta-e website scripts
