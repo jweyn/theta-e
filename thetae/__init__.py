05bf057721d4b4892ef7034efabdc3a1bd48b006
jweyn <33530169+jweyn@users.noreply.github.com>
2017-11-13 10:45:58 -0800

    Update __init__.py

--- thetae/__init__.py
+++ thetae/__init__.py
@@ -40,3 +40,33 @@ def getConfig(config_path):
         print("Using configuration file %s" % config_path)
 
     return config_dict
+
+# =============================================================================
+# Classes
+# =============================================================================
+
+import numpy as np
+import pandas as pd
+
+class TimeSeries():
+    def __init__(self):
+        self.data = pd.DataFrame()
+
+class Daily():
+    def __init__(self):
+        self.high = np.nan
+        self.low = np.nan
+        self.wind = np.nan
+        self.rain = np.nan
+
+class Forecast(stid, date, source):
+    '''
+    Forecast object for a single date. Contains both a timeseries and daily values.
+    '''
+    
+    def __init__(self):
+        self.stid = stid
+        self.source = source
+        self.date = date
+        self.timeseries = TimeSeries()
+        self.daily = Daily()
