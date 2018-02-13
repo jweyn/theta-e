c12d8cc8d8f3fe2c83e376a6892a909c96e0ba96
joejoezz <joejoezz@gmail.com>
2018-02-13 11:51:36 -0800

    ukmet module finished for both daily (next day only) and timeseries (+3 days)

--- thetae/data_parsers/ukmet.py
+++ thetae/data_parsers/ukmet.py
@@ -7,8 +7,8 @@
 """
 Retrieve UKMET data
 
-Currently only handles daily high and low
-To do: add wind and hourly data
+Daily: high, low, wind speed
+Hourly: temp, dewpt, wind speed, wind gust, wind direction
 """
 
 from thetae import Forecast
@@ -18,7 +18,7 @@ import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
 from thetae.util import c_to_f, mph_to_kt, wind_dir_to_deg
-import pdb
+from selenium import webdriver 
 
 default_model_name = 'UKMET'
 
@@ -56,19 +56,111 @@ def get_ukmet_forecast(stid, ukmet_code, init_date, forecast_date):
 
     #store daily variables 
     days = []
-    highs = []
-    lows = []
-    wspds = []
+    highs = [] #this can be overwritten by hourly
+    lows = [] #this can be overwritten by hourly
+    wspds = [] #this comes from hourly
 
-    #First part: li tages for "tabDay1, tabDay2" (tomorrow, next day high and low)
-    tabids = ['tabDay1','tabDay2','tabDay3','tabDay4']
+    #Pull in daily data using li tabs
+    tabids = ['tabDay1','tabDay2','tabDay3']
     for ids in tabids:
         pars = soup.find(id=ids)
         days.append(datetime.strptime(pars['data-date'],'%Y-%m-%d')) 
         highs.append(c_to_f(pars.findAll("span", { "title" : "Maximum daytime temperature" })[0]['data-value-raw']))
         lows.append(c_to_f(pars.findAll("span", { "title" : "Minimum nighttime temperature" })[0]['data-value-raw']))
 
-    #Make list of forecast objects
+    #Pull in hourly data
+    #This requires PhantomJS to pull out additional HTML code
+    driver = webdriver.PhantomJS(executable_path='/home/disk/p/wxchallenge/bin/phantomjs')
+    driver.get(url+'#?date=2017-09-21')
+    source = driver.page_source
+    soup = BeautifulSoup(source,'html.parser')
+
+    dateTime = []
+    temperature = []
+    temperature_c = []
+    dewpoint = []
+    windSpeed = []
+    windGust = []
+    windDir = []
+    humidity = [] #this is temporary--converted to dew point below
+
+    divids = ['divDayModule0','divDayModule1','divDayModule2','divDayModule3']
+    for i,divs in enumerate(divids):
+        day0 = datetime.strptime(soup.find("div", { "id" : "divDayModule0" })['data-content-id'],'%Y-%m-%d')
+        day1 = (day0+timedelta(days=1)).strftime('%Y-%m-%d')
+        pars = soup.find(id=divs)
+        divdate = datetime.strptime(pars['data-content-id'],'%Y-%m-%d').date()
+        hourels = pars.findAll("tr", { "class" : "weatherTime" })[0].find_all('td')
+        for ii,ele in enumerate(hourels):
+            if ele.text == 'Now':
+                dateTime.append(utcnow)
+            else:
+                dtmp = datetime(divdate.year,divdate.month,divdate.day,int(ele.text.split(':')[0]),int(ele.text.split(':')[1]))
+                dateTime.append(dtmp+timedelta(hours=utcoffset))
+        tempels = pars.findAll("tr", { "class" : "weatherTemp" })[0].findAll("i", { "class" : "icon icon-animated" })
+        for ele in tempels:
+            temperature_c.append(float(ele['data-value-raw']))
+            temperature.append(c_to_f(ele['data-value-raw']))
+        #relative humidity for conversion to dew point
+        humels = pars.findAll("tr", { "class" : "weatherHumidity" })[0].text.split() 
+        for ele in humels:
+            humidity.append(float(ele.split('%')[0]))
+        #add wind 
+        speedels = pars.findAll("i",{ "data-type" : "windSpeed"})  
+        for ele in speedels:
+            windSpeed.append(float(ele['data-value-raw'])*0.868976)
+        gustels = pars.findAll("span",{ "class" : "gust"})  
+        for ele in gustels:
+            windGust.append(float(ele['data-value-raw'])*0.868976)
+        direls = pars.findAll("span",{ "class" : "direction"})  
+        for ele in direls:
+            windDir.append(wind_dir_to_deg(ele.text))
+
+    #convert T and humidity to dewpt
+    for ii,thum in enumerate(humidity):
+        td_tmp = 243.04*(np.log(thum/100)+((17.625*temperature_c[ii])/(243.04+temperature_c[ii])))/(17.625-np.log(thum/100)-((17.625*temperature_c[ii])/(243.04+temperature_c[ii])))
+        dewpoint.append(c_to_f(td_tmp))
+
+    #Make into dataframe
+    df = pd.DataFrame({ 'temperature' : temperature,
+                        'dewpoint' : dewpoint,
+                        'windSpeed' : windSpeed,
+                        'windGust' : windGust,
+                        'windDir' : windDir},index = dateTime)
+
+    #Correct the highs and lows with the hourly data, find max wind speed
+    forecast_start = forecast_date.replace(hour=6)
+    forecast_end = forecast_start + timedelta(days=1)
+    for d in range(0,len(days)):
+        try:
+            #unlike the mos code, we always use the 'include'
+            iloc_start_include = df.index.get_loc(forecast_start)
+        except BaseException:
+            print('Error getting start time index in db; check data.')
+            break
+        try:
+            iloc_end = df.index.get_loc(forecast_end)
+        except BaseException:
+            print('Error getting end time index in db; check data.')
+            break
+        raw_high = df.iloc[iloc_start_include:iloc_end]['temperature'].max()
+        raw_low = df.iloc[iloc_start_include:iloc_end]['temperature'].min()
+        wspds.append(int(np.round(df.iloc[iloc_start_include:iloc_end]['windSpeed'].max())))
+        if raw_high > highs[d]:
+            highs[d] = raw_high
+        if raw_low < lows[d]:
+            lows[d] = raw_low
+        forecast_start = forecast_start + timedelta(days=1)
+        forecast_end = forecast_end + timedelta(days=1)
+
+    forecast = Forecast(stid, default_model_name, days[0])        
+    forecast.timeseries.data = df
+    forecast.daily.high = highs[0]
+    forecast.daily.low = lows[0]
+    forecast.daily.wspd = wspds[0]
+
+    #Make list of forecast objects for future days--currently not implemented
+    '''
     forecast = []
 
     for i in range(0,len(days)):
@@ -77,8 +169,8 @@ def get_ukmet_forecast(stid, ukmet_code, init_date, forecast_date):
         forecast_tmp.daily.high = highs[i]
         forecast_tmp.daily.low = lows[i]
         forecast.append(forecast_tmp)
-
-    return forecast[0]
+    '''
+    return forecast
 
 
 def main(config, model, stid, forecast_date):
