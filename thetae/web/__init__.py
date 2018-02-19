56a0203b1316ab986e0ba4d089faf4e91fea1ead
jweyn <jweyn@uw.edu>
2018-02-19 13:42:28 -0800

    Restructured output modules

---
+++ thetae/web/__init__.py
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2018 Jonathan Weyn <jweyn@uw.edu>
+#
+# See the file LICENSE for your rights.
+#
+
+"""
+Module containing plotting functions.
+
+Each sub-module should have a function main() which takes in arguments of (config, model, stid, forecast_date). The
+module 'all' calls all of the plotting modules specified in the config, and also contains a historical function to
+execute any historical plots.
+"""
+
+# =============================================================================
+# It may no longer be necessary to do this import, based on the change to util.get_object
+# =============================================================================
+
+# import os
+#
+# for module in os.listdir(os.path.dirname(__file__)):
+#     if module == '__init__.py' or module[-3:] != '.py':
+#         continue
+#     try:
+#         __import__(module[:-3], locals(), globals())
+#     except BaseException as e:
+#         print("data_parsers __init__.py: error importing %s" % module[:-3])
+#         print("*** Reason: '%s'" % str(e))
+# del module
