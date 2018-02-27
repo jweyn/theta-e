871e3c2451911345736fa5b7d55ab900fb54ef2f
jweyn <jweyn@uw.edu>
2018-02-27 13:20:50 -0800

    Minor edits to gefs_mos.py

--- thetae/data_parsers/gefs_mos.py
+++ thetae/data_parsers/gefs_mos.py
@@ -10,10 +10,10 @@ Retrieve GEFS MOS data (ensemble means)
 Daily: high, low, precip (no wind speed)
 Hourly: none
 
-Does not currently store information from the individual ensemble
-members but this feature could easily be added in the future.
+Does not currently store information from the individual ensemble members but this feature could easily be added in the
+future.
 
-GEFS MOS does not account for the 06-06 UTC timeframe.
+GEFS MOS does not account for the 06-06 UTC time frame.
 """
 
 import re
@@ -22,7 +22,9 @@ from datetime import datetime, timedelta
 import urllib2
 import numpy as np
 from bs4 import BeautifulSoup
-from thetae.util import get_codes, c_to_f, mph_to_kt, wind_dir_to_deg, dewpoint_from_t_rh
+
+default_model_name = 'GEFS MOS'
+
 
 def mos_qpf_interpret(qpf):
     """
@@ -44,7 +46,6 @@ def mos_qpf_interpret(qpf):
 
     return new_qpf
 
-default_model_name = 'GEFS MOS'
 
 def get_gefs_mos_forecast(stid, forecast_date):
     """
@@ -60,7 +61,7 @@ def get_gefs_mos_forecast(stid, forecast_date):
     req = urllib2.Request(url) 
     response = urllib2.urlopen(req)
     page = response.read().decode('utf-8', 'ignore')
-    soup = BeautifulSoup(page,'html.parser')
+    soup = BeautifulSoup(page, 'html.parser')
 
     # Lists for tomorrow's ensemble data
     ens_highs = []  
@@ -69,43 +70,43 @@ def get_gefs_mos_forecast(stid, forecast_date):
 
     # 22 total model runs
     pars = soup.find_all('pre')
-    for ii in range(0,len(pars)-1): # last one is operational run...don't use that
+    for ii in range(0, len(pars) - 1):  # last one is operational run... don't use that
         text = pars[ii].text.split()
         # control run
         if ii == 0:
             # get model time
             dates = text[5].split('/')
             hour = int(text[6])
-            modeltime = datetime(int(dates[2]),int(dates[0]),int(dates[1]),hour)
+            model_time = datetime(int(dates[2]), int(dates[0]), int(dates[1]), hour)
         # find all of the forecast hours (every 12 hr)
-        fhrs_tmp = pars[ii].text.split('FHR')[1].split('    ')[0]
-        fhrs = map(int,re.findall(r'\d+',fhrs_tmp))
+        forecast_hours_tmp = pars[ii].text.split('FHR')[1].split('    ')[0]
+        forecast_hours = map(int, re.findall(r'\d+', forecast_hours_tmp))
         # find all of the temperatures that match the forecast hours
-        ftmps_tmp = pars[ii].text.split('X/N')[1].split('    ')[0]
-        ftmps = map(int,re.findall(r'\d+',ftmps_tmp))
+        forecast_temps_tmp = pars[ii].text.split('X/N')[1].split('    ')[0]
+        forecast_temps = map(int, re.findall(r'\d+', forecast_temps_tmp))
         # find all of the 24 hour precips
-        fpcp_tmp = pars[ii].text.split('Q24')[1].split('|')[1]
-        fpcp = map(int,re.findall(r'\d+',fpcp_tmp))[0:5]
+        forecast_precip_tmp = pars[ii].text.split('Q24')[1].split('|')[1]
+        forecast_precip = map(int, re.findall(r'\d+', forecast_precip_tmp))[0:5]
         
-        fdates_utc = []
+        forecast_dates_utc = []
         temps = []
         
-        for ii2,fh in enumerate(fhrs):
+        for f, forecast_hour in enumerate(forecast_hours):
             # append forecast time, but subtract 1 hour so the 00Z time is for the correct date
-            fdates_utc.append((modeltime+timedelta(hours=fh-1)).date())
-            temps.append(ftmps[ii2]) 
-        fdates_utc = np.array(fdates_utc)
+            forecast_dates_utc.append((model_time + timedelta(hours=forecast_hour-1)).date())
+            temps.append(forecast_temps[f])
+        forecast_dates_utc = np.array(forecast_dates_utc)
         temps = np.array(temps)
-        gdates = np.where((fdates_utc == forecast_date.date()))[0]
-        ens_highs.append(np.max(temps[gdates]))
-        ens_lows.append(np.min(temps[gdates]))
+        valid_dates = np.where((forecast_dates_utc == forecast_date.date()))[0]
+        ens_highs.append(np.max(temps[valid_dates]))
+        ens_lows.append(np.min(temps[valid_dates]))
         # the 24 hour precip for the next day is always the first value
-        ens_precips.append(mos_qpf_interpret(fpcp[0]))
+        ens_precips.append(mos_qpf_interpret(forecast_precip[0]))
 
     # get ensemble mean:
-    high_mean = int(np.round(np.mean(ens_highs)))
-    low_mean = int(np.round(np.mean(ens_lows)))
-    precip_mean = np.around(np.mean(ens_precips),2)
+    high_mean = np.round(np.mean(ens_highs))
+    low_mean = np.round(np.mean(ens_lows))
+    precip_mean = np.round(np.mean(ens_precips), 2)
 
     # create forecast object 
     forecast = Forecast(stid, default_model_name, forecast_date)        
