63c5baaa4bfc69f19b0dded6b38f94f58db43036
jweyn <jweyn@uw.edu>
2019-02-19 11:27:16 -0800

    Added HRRR AK check in bufkit.py

--- thetae/data_parsers/bufkit.py
+++ thetae/data_parsers/bufkit.py
@@ -45,6 +45,8 @@ def get_bufkit_forecast(config, bufr, bufkit_dir, model, bufr_name, cycle, stid,
     bufr_search_model = re.search(r'[^0-2]*', model).group().lower()
     if bufr_search_model == 'fv3':
         bufr_search_model = 'fv3gfsx'
+    if bufr_search_model == 'hrrr' and stid.upper()[0] == 'P':
+        bufr_search_model = 'hrrrak'
     bufr_file_name = '%s/bufkit/%s.%s_%s.buf' % (bufkit_dir, model_time, bufr_name, stid.lower())
 
     # Check if bufkit file was already downloaded
