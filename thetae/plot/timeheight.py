43ea80724f9f1f58ebad7ac6862af5768a05acca
joejoezz <joejoezz@gmail.com>
2019-08-07 15:08:09 -0700

    updates to timeheight function

--- thetae/plot/timeheight.py
+++ thetae/plot/timeheight.py
@@ -6,7 +6,6 @@
 
 """
 Generates bufkit timeheight plots for various models.
-
 """
 
 import os
@@ -15,12 +14,12 @@ import re
 import numpy as np
 from scipy import interpolate
 import pandas as pd
-from thetae.db import readTimeSeries
+from scipy.ndimage.filters import gaussian_filter
+from math import ceil
 from datetime import datetime, timedelta
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
-import pdb #depete later
 
 
 def plot_timeheight(config, stid, model, forecast_date, variable, df, plot_dir, img_type):
@@ -30,37 +29,89 @@ def plot_timeheight(config, stid, model, forecast_date, variable, df, plot_dir, 
     Currently designed to convert the Profile to a Pandas MultiIndex which is easy to manipulate for plotting.
     Future implementations should probably get rid of the Profile OrderedDict construct altogether.
     """
-    # can get rid of this
-    var_names = ['temperature', 'dewPointDep', 'cloud', 'windSpeed', 'omega']
 
-    # get forecast surface pressure trace
-    # if this fails then presumably the bufkit data isn't available for this model run yet
-    try:
-        fcst = readTimeSeries(config, stid, 'forecast', 'hourly_forecast', model=model,
-                              start_date=pd.to_datetime(df.columns.values[0]),
-                              end_date=pd.to_datetime(df.columns.values[-1]))
-    except ValueError:
-        if config['debug'] > 9:
-            print('plot.timeheight warning: no hourly timeseries data for %s, %s' % (stid, model))
-
-    pres_surf = pd.Series(fcst.data['PRESSURE'].values, index=fcst.data['DATETIME'])
+    # vertical pressure levels (y-coordinates)
+    p_levels = np.flip(np.unique(df.index.get_level_values('pressure').values), 0)
 
     # slice dataframe depending on which variable we are plotting
+    # select plotting variables, exclude models that do not have certain variables
     idx = pd.IndexSlice
     if variable == 'temperature':
         temperature = df.loc[idx[:, 'TMPC'], :]
         cmap = 'jet'
-        vrange = [-20,25]
+        vrange = [-20, 25]
+        ytop = 650
         plot_variable = np.ma.masked_array(temperature.values.astype('float'))
+        extend = 'both'
+    elif variable == 'dewPointDep':
+        dewpoint_dep = df.loc[idx[:, 'TMPC'], :].values - df.loc[idx[:, 'DWPC'], :].values
+        cmap = 'BrBG_r'
+        vrange = [0, 10]
+        ytop = 200
+        extend = 'max'
+        plot_variable = np.ma.masked_array(dewpoint_dep.astype('float'))
+    elif variable == 'cloud' and model[0:3] != 'GFS' and model[0:4] != 'HRRR':
+        cloud_fr = df.loc[idx[:, 'CFRL'], :]
+        cmap = 'Blues_r'
+        vrange = [0, 100]
+        ytop = 200
+        plot_variable = np.ma.masked_array(cloud_fr.values.astype('float'))
+        extend = 'neither'
+    elif variable == 'windSpeed':
+        uwnd = df.loc[idx[:, 'UWND'], :].values.astype('float')
+        vwnd = df.loc[idx[:, 'VWND'], :].values.astype('float')
+        sknt = np.sqrt(uwnd ** 2 + vwnd ** 2)
+        drct = np.ma.masked_array(180 / np.pi * np.arctan2(uwnd, vwnd))
+        drct = np.ma.masked_where(np.isnan(drct), drct)
+        drct += 180
+        drct[np.where((drct < 0))] += 360
+        cmap = 'jet'
+        ytop = 650
+        vrange = [0, int(ceil(np.nanmax(sknt[0:np.where((p_levels == ytop))[0][0], :] / 10.0))) * 10]
+        extend = 'neither'
+        plot_variable = sknt
 
-    # get x and y values
-    times = df.loc[idx[:, 'TMPC'], :].columns
-    p_levels = np.flip(np.unique(df.index.get_level_values('pressure').values),0)
+        # select values for wind barbs
+        barb_heights = [1000., 950., 900., 850., 800., 750., 700.]
+        uwnd_barbs = np.zeros((len(barb_heights), len(uwnd[0,:])))
+        vwnd_barbs = np.zeros((len(barb_heights), len(uwnd[0,:])))
+        for i in range(0, len(barb_heights)):
+            uwnd_barbs[i, :] = uwnd[np.where((p_levels == barb_heights[i])), :]
+            vwnd_barbs[i, :] = vwnd[np.where((p_levels == barb_heights[i])), :]
+
+        # get boundary-level winds
+        bl_df = compute_bl_winds(df)
+    elif variable == 'omega':
+        omeg = df.loc[idx[:, 'OMEG'], :].values.astype('float')
+        cmap = 'seismic'
+        ytop = 200
+        vrange = [-ceil(np.nanmax(np.abs(omeg)*10))/10., ceil(np.nanmax(np.abs(omeg)*10))/10.]
+        extend = 'neither'
+        plot_variable = omeg
+
+        # also plot potential temperature contours on omega
+        tmpk = df.loc[idx[:, 'TMPC'], :].values.astype('float') + 273.15
+        pres = df.loc[idx[:, 'PRES'], :].values.astype('float')
+        theta = tmpk * (1000 / pres) ** 0.286
+    else:
+        if config['debug'] > 50:
+            print('%s Bufkit time-height data NOT processed--VARIABLE: %s for MODEL = %s' % (stid, variable, model))
+        return
+
+    if config['debug'] > 50:
+        print('%s Bufkit time-height data processed--VARIABLE: %s for MODEL = %s' % (stid, variable, model))
+
+    # model times (x-values)
+    times = pd.to_datetime(df.loc[idx[:, 'TMPC'], :].columns)
 
     # set up the plot
     fig = plt.figure()
     fig.set_size_inches(8, 6)
     ax = fig.add_subplot(1, 1, 1)
+
+    # get lower y limit by figuring out which pressure surfaces have data
+    pres_surf = np.array(zip(*df.loc[idx[:, 'TMPC'], :].apply(pd.Series.first_valid_index).values.tolist())[0])
+    ylims = (np.max(pres_surf), ytop)
 
     # meshplot of variable
     plot_variable = np.ma.masked_where(np.isnan(plot_variable), plot_variable)
@@ -72,13 +123,27 @@ def plot_timeheight(config, stid, model, forecast_date, variable, df, plot_dir, 
         zero_line = plt.contour(times, p_levels, plot_variable, [0.0], colors='k', linewidths=3,
                                 linestyles='dashed')
         plt.clabel(zero_line, colors='k', inline_spacing=1, fmt='%1.0f C', rightside_up=True)
-
-    # shade the area black below the pressure "surface"
-    #ax.fill_between(pres_surf.index, pres_surf.values, [ax.get_ylim()[1]]*len(pres_surf.values),facecolor='saddlebrown')
+    # plot wind barbs and mixed-layer height on wind plot
+    if variable == 'windSpeed':
+        plt.barbs(matplotlib.dates.date2num(times.to_pydatetime()), barb_heights, uwnd_barbs, vwnd_barbs, length=5.5,
+                  lw=0.5)
+        bl_line = ax.plot(bl_df.index, bl_df['pressure'], color='red', linewidth=2, linestyle='dashed')
+        plt.legend([bl_line[0]], ['Mixed Layer Height'], loc=2)
+    if variable == 'omega':
+        theta_contours = np.arange(0,402,2)
+        theta_lines = plt.contour(times, p_levels, theta, theta_contours, colors='k', linewidths=1,linestyles='solid')
+        plt.clabel(theta_lines, colors='k', inline_spacing=1, fmt='%d', rightside_up=True, fontsize=8)
+        h1, _ = theta_lines.legend_elements()
+        plt.legend([h1[0]], ['Potential Temperature (K)'], loc=2, framealpha=0.9)
 
     # vertical lines at start and end of forecast period
     ax.plot([forecast_date + timedelta(hours=6)]*2, [p_levels[0], p_levels[-1]], color='k', lw=2.0)
     ax.plot([forecast_date + timedelta(hours=30)]*2, [p_levels[0], p_levels[-1]], color='k', lw=2.0)
+    # label end of model run if it is located on plot
+    if times[-1] < (forecast_date + timedelta(hours=40)):
+        ax.plot([times[-1]] * 2, [p_levels[0], p_levels[-1]], color='magenta', lw=2.0)
+        ax.text(times[-1]+timedelta(hours=1), np.mean(ylims), '(end of model run)', color='magenta', rotation=90,
+                ha='center', va='center')
 
     # Plot configurations and saving
     ax.grid()
@@ -90,26 +155,21 @@ def plot_timeheight(config, stid, model, forecast_date, variable, df, plot_dir, 
     from matplotlib import dates
     from mpl_toolkits.axes_grid1 import make_axes_locatable
     ax.set_xlim(times[0],forecast_date+timedelta(hours=42))
-    ax.set_ylim(1020,450)
+    ax.set_ylim(ylims)
     ax.xaxis.set_major_locator(dates.HourLocator(byhour=list(range(0, 25, 3))))
     ax.xaxis.set_major_formatter(dates.DateFormatter('%HZ'))
     ax.xaxis.set_minor_locator(dates.DayLocator())
     ax.xaxis.set_minor_formatter(dates.DateFormatter('%h %d'))
     ax.xaxis.set_tick_params(which='minor', pad=15)
 
+    # colorbar
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", size="3%", pad=0.15)
-    plt.colorbar(meshplot, cax)
+    plt.colorbar(meshplot, cax, extend=extend)
 
     # Save plot
-    plt.savefig('{}/{}_timeheight_{}.{}'.format(plot_dir, stid, variable.upper(), img_type), dpi=150)
-
-
-
-    pdb.set_trace()
-
-
-    pdb.set_trace()
+    plt.savefig('{}/{}_timeheight_{}_{}.{}'.format(plot_dir, stid, variable.upper(), model, img_type), dpi=150)
+    plt.close()
     return
 
 
@@ -249,8 +309,47 @@ def bufr_timeheight_parser(config, model, stid, forecast_date):
     for var in bufr_vars:
         for dt in profile.keys():
             df[dt].loc[:, var] = np.array(profile[dt][var])
-
     return df
+
+
+def compute_bl_winds(bufkit_df):
+    """
+    Computes mixed-layer height and mixed-layer winds from bufkit profiles
+    Works by searching for the height where the lapse rate is no longer close to dry adiabatic
+    Function written by Luke Madaus, modified by Joe Zagrodnik
+
+    :param bufkit_df: dataframe generated by bufr_timeheight_parser function
+    :return: dataframe of mixed-layer height, mean and max mixed-layer wind
+
+    """
+    idx = pd.IndexSlice
+    tmpc = bufkit_df.loc[idx[:, 'TMPC'], :].values.astype('float')
+    hght = bufkit_df.loc[idx[:, 'HGHT'], :].values.astype('float')
+    uwnd = bufkit_df.loc[idx[:, 'UWND'], :].values.astype('float')
+    vwnd = bufkit_df.loc[idx[:, 'VWND'], :].values.astype('float')
+    sknt = np.sqrt(uwnd ** 2 + vwnd ** 2)
+    times = pd.to_datetime(bufkit_df.loc[idx[:, 'TMPC'], :].columns)
+    p_levels = np.flip(np.unique(bufkit_df.index.get_level_values('pressure').values), 0)
+
+    # Compute the gradients of height and temperature
+    dh = np.gradient(hght, axis=0)
+    dt = np.gradient(tmpc, axis=0)
+    dh_km = dh / 1000.
+    dtdz = np.divide(dt, dh_km)
+
+    # dataframe to store boundary layer wind values
+    bl_df = pd.DataFrame(index=times, columns=['height', 'pressure', 'mean_wind', 'max_wind'])
+
+    for i in range(0, len(dtdz[0,:])):
+        # free atmosphere starts at first level where dT/dZ > -6.0 degC/km
+        free_atm = np.where((dtdz[:, i] > -6.0))[0]
+        b_loc = np.min(free_atm)
+        bl_df['height'][i] = hght[b_loc, i]
+        bl_df['pressure'][i] = p_levels[b_loc]
+        bl_df['mean_wind'][i] = np.nanmean(sknt[b_loc, i])
+        bl_df['max_wind'][i] = np.nanmax(sknt[b_loc, i])
+
+    return bl_df
 
 
 def main(config, stid, forecast_date):
