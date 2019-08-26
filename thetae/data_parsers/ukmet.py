e1d5bd0b17462c75f0fdd832ebfc8b0b2eee4fa8
jweyn <jweyn@uw.edu>
2019-08-26 14:36:01 -0700

    Add UKMET data caching

--- thetae/data_parsers/ukmet.py
+++ thetae/data_parsers/ukmet.py
@@ -10,14 +10,17 @@ Retrieve UKMET data
 """
 
 from thetae import Forecast
-from thetae.util import c_to_f, ms_to_kt, mm_to_in
+from thetae.util import c_to_f, ms_to_kt, mm_to_in, check_cache_file
 from datetime import timedelta
 import requests
 import pandas as pd
+import json
 
 default_model_name = 'UKMET'
 
-def get_ukmet_forecast(stid, lat, lon, api_id, api_secret, forecast_date):
+
+def get_ukmet_forecast(config, stid, lat, lon, api_id, api_secret, forecast_date):
+    json_url = 'https://api-metoffice.apiconnect.ibmcloud.com/metoffice/production/v0/forecasts/point'
     headers = {
         'x-ibm-client-id': api_id,
         'x-ibm-client-secret': api_secret,
@@ -31,16 +34,26 @@ def get_ukmet_forecast(stid, lat, lon, api_id, api_secret, forecast_date):
         'longitude': lon,
     }
 
-    json_url = 'https://api-metoffice.apiconnect.ibmcloud.com/metoffice/production/v0/forecasts/point'
-    response = requests.get('%s/hourly' % json_url, params=api_options, headers=headers)
-    ukmet_data_hourly = response.json()
+    # Get hourly forecast data
+    # Check if we have a cached hourly file and if it is recent enough
+    site_directory = '%s/site_data' % config['THETAE_ROOT']
+    cache_file = '%s/%s_ukmet_hourly.txt' % (site_directory, stid)
+    cache_ok = check_cache_file(config, cache_file, interval=4)
 
-    # Raise error for invalid HTTP response
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        print('ukmet: got HTTP error when querying API for hourly data')
-        raise
+    if not cache_ok:
+        response = requests.get('%s/hourly' % json_url, params=api_options, headers=headers)
+        ukmet_data_hourly = response.json()
+        # Raise error for invalid HTTP response
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            print('ukmet: got HTTP error when querying API for hourly data')
+            raise
+        # Cache the response
+        with open(cache_file, 'w') as f:
+            f.write(response.text)
+    else:
+        ukmet_data_hourly = json.load(open(cache_file))
 
     # model run date--currently not using this but might be of interest later
     model_run_date = ukmet_data_hourly['features'][0]['properties']['modelRunDate']
@@ -81,25 +94,39 @@ def get_ukmet_forecast(stid, lat, lon, api_id, api_secret, forecast_date):
     forecast_start = forecast_date.replace(hour=6)
     forecast_end = forecast_start + timedelta(days=1)
 
-    # find daily values
-    json_url = 'https://api-metoffice.apiconnect.ibmcloud.com/metoffice/production/v0/forecasts/point'
-    response = requests.get('%s/daily' % json_url, params=api_options, headers=headers)
-    ukmet_data_daily = response.json()
+    # Now use the daily API to find daily values
+    # Check if we have a cached daily file and if it is recent enough
+    site_directory = '%s/site_data' % config['THETAE_ROOT']
+    cache_file = '%s/%s_ukmet_daily.txt' % (site_directory, stid)
+    cache_ok = check_cache_file(config, cache_file, interval=4)
 
-    # Raise error for invalid HTTP response
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        print('ukmet: got HTTP error when querying API for daily data')
-        raise
+    have_daily_values = True
+    if not cache_ok:
+        response = requests.get('%s/daily' % json_url, params=api_options, headers=headers)
+        ukmet_data_daily = response.json()
+        # Raise error for invalid HTTP response
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            print('ukmet warning: got HTTP error when querying API for daily data; using hourly values')
+            have_daily_values = False
+        # Cache the response
+        with open(cache_file, 'w') as f:
+            f.write(response.text)
+    else:
+        ukmet_data_daily = json.load(open(cache_file))
 
     # extract daily values for the forecast date
-    ukmet_df_daily = pd.DataFrame(ukmet_data_daily['features'][0]['properties']['timeSeries'])
-    ukmet_df_daily.set_index('time', inplace=True)
-    ukmet_df_daily.index = pd.to_datetime(ukmet_df_daily.index)
-    daily_forecast = ukmet_df_daily.loc[forecast_date]
-    daytime_max = c_to_f(daily_forecast['dayMaxScreenTemperature'])
-    nighttime_min = c_to_f(daily_forecast['nightMinScreenTemperature'])
+    if have_daily_values:
+        ukmet_df_daily = pd.DataFrame(ukmet_data_daily['features'][0]['properties']['timeSeries'])
+        ukmet_df_daily.set_index('time', inplace=True)
+        ukmet_df_daily.index = pd.to_datetime(ukmet_df_daily.index)
+        daily_forecast = ukmet_df_daily.loc[forecast_date]
+        daytime_max = c_to_f(daily_forecast['dayMaxScreenTemperature'])
+        nighttime_min = c_to_f(daily_forecast['nightMinScreenTemperature'])
+    else:
+        daytime_max = -1000.
+        nighttime_min = 1000.
 
     # compare hourly temperature to daily--update if needed
     daily_high = ukmet_df.loc[forecast_start:forecast_end, 'temperature'].max()
@@ -141,6 +168,6 @@ def main(config, model, stid, forecast_date):
         raise KeyError('ukmet: no api_secret parameter defined for model %s in config!' % model)
 
     # Get forecast
-    forecast = get_ukmet_forecast(stid, lat, lon, api_id, api_secret, forecast_date)
+    forecast = get_ukmet_forecast(config, stid, lat, lon, api_id, api_secret, forecast_date)
 
     return forecast
